5329a324c3760d735f39c2584b91adf551bda1f9
Ole Sund <ole.r.sund@gmail.com>
2025-11-15 14:41:46 +0100

    testing

--- packages/rrweb/src/mutation-rate-limiter.ts
+++ packages/rrweb/src/mutation-rate-limiter.ts
@@ -10,7 +10,13 @@ class MutationRateLimiter {
     ts: number;
   };
 
+  exitMutTracker: {
+    muts: number;
+    requested: number;
+  };
+
   interval = 50;
+  exitInterval = 100;
   limit = 100;
   inGlobalStorm = false;
 
@@ -25,28 +31,88 @@ class MutationRateLimiter {
     this.reset();
   }
 
-  reset() {
+  resetTracker() {
     this.mutTracker = {
       muts: 0,
       ts: -1,
     };
   }
 
+  resetExitTracker() {
+    this.exitMutTracker = {
+      muts: 0,
+      requested: -1,
+    };
+  }
+
+  reset() {
+    this.resetTracker();
+    this.resetExitTracker();
+  }
+
+  handleStormExit(muts: number) {
+    const now = Date.now();
+
+    if (this.exitMutTracker.requested === -1) {
+      this.exitMutTracker = {
+        requested: now,
+        muts: muts,
+      };
+    } else {
+      this.exitMutTracker.muts += muts;
+
+      if (now - this.exitMutTracker.requested > this.exitInterval) {
+        if (this.exitMutTracker.muts >= this.limit) {
+          //continue to storm, do not exit
+          debugLog(
+            `MutationRateLimiter, exit cooldown failed, continuing with storm`,
+            {
+              mutTracker: this.mutTracker,
+              exitMutTracker: this.exitMutTracker,
+            },
+          );
+
+          this.mutTracker.ts = now;
+          this.mutTracker.muts += this.exitMutTracker.muts;
+
+          this.resetExitTracker();
+
+          return true;
+        } else {
+          //storm is over, exit
+
+          debugLog(`MutationRateLimiter, detected global storm exit.`, {
+            mutTracker: this.mutTracker,
+            exitMutTracker: this.exitMutTracker,
+          });
+
+          this.inGlobalStorm = false;
+          this.reset();
+
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
   public isStorming(muts: number) {
     const now = Date.now();
 
     if (this.inGlobalStorm) {
-      this.mutTracker.muts += muts;
-
       if (now - this.mutTracker.ts > this.interval) {
-        this.inGlobalStorm = false;
-        debugLog(
-          `MutationRateLimiter, detected global storm over. Total mutations stormed: ${this.mutTracker.muts}`,
-        );
-        this.reset();
-        return false;
+        return this.handleStormExit(muts);
+
+        // this.inGlobalStorm = false;
+        // debugLog(
+        //   `MutationRateLimiter, detected global storm over. Total mutations stormed: ${this.mutTracker.muts}`,
+        // );
+        // this.reset();
+        // return false;
       }
 
+      this.mutTracker.muts += muts;
       this.mutTracker.ts = now;
       return true;
     }
